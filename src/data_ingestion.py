b0580a3aacf379578ae1683b7180679b820566ad
Chebrolu Trilokesh Venkata Uday <trilokeshvenkatauday@gmail.com>
2025-07-11 03:38:02 +0530

    Done

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -35,3 +35,29 @@ def preprocess_data(df: pd.DataFrame) -> pd.DataFrame:
         raise
 
 
+
+
+def save_data(train_data: pd.DataFrame, test_data: pd.DataFrame, data_path: str) -> None:
+    try:
+        data_path = os.path.join(data_path, 'raw')
+        os.makedirs(data_path, exist_ok=True)
+        train_data.to_csv(os.path.join(data_path, "train.csv"), index=False)
+        test_data.to_csv(os.path.join(data_path, "test.csv"), index=False)
+    except Exception as e:
+        print(f"Error: An unexpected error occurred while saving the data.")
+        print(e)
+        raise
+
+
+def main():
+    try:
+        df = load_data(data_url='https://raw.githubusercontent.com/entbappy/Branching-tutorial/refs/heads/master/tweet_emotions.csv')
+        final_df = preprocess_data(df)
+        train_data, test_data = train_test_split(final_df, test_size=0.2, random_state=42)
+        save_data(train_data, test_data, data_path='data')
+    except Exception as e:
+        print(f"Error: {e}")
+        print("Failed to complete the data ingestion process.")
+
+if __name__ == '__main__':
+    main()
